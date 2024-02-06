89f2bcd76bc958f77ceacaec22a8b34ab8d3caec
owenavon <89999663+owenavon@users.noreply.github.com>
2024-02-06 12:27:52 -0500

    Attempted to ad JS to show and hide, which will be used to display different style components.
    Each section will show an example of what is to be used, and a code example.

---
+++ Design System/js/test.js
@@ -0,0 +1,10 @@
+// Show and hide site sections
+function yesButton() {
+  document.getElementById('yesChoice').style.display ='block'; // Displays "Yes" choice code block
+  document.getElementById('noChoice').style.display ='none'; // Hides "No" choice code block
+}
+
+function noButton() {
+  document.getElementById('noChoice').style.display = 'block'; // Displays "No" choice code block
+  document.getElementById('yesChoice').style.display ='none'; // Hides "Yes" choice code block
+}
