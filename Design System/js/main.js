f8cb1ccecba3abd3ef86033fd041c94c546e7883
owenavon <89999663+owenavon@users.noreply.github.com>
2024-02-06 14:25:45 -0500

    Removed JS show and hide. Added JQuery Accordion

--- Design System/js/main.js
+++ Design System/js/main.js
@@ -107,20 +107,11 @@ jQuery(document).ready(function($){
 			}
 		});
 	}
-});
-
-
-
-
-
-// Show and hide site sections
-    function yesButton() {
-      document.getElementById('yesChoice').style.display ='block'; // Displays "Yes" choice code block
-      document.getElementById('noChoice').style.display ='none'; // Hides "No" choice code block
-    }
-
-    function noButton() {
-      document.getElementById('noChoice').style.display = 'block'; // Displays "No" choice code block
-      document.getElementById('yesChoice').style.display ='none'; // Hides "Yes" choice code block
-    }
     
+    
+    // Accordion - Show and hide
+    $( "#accordion" ).accordion({
+        collapsible: true,
+        active: false
+    });
+});
