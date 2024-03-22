7b632f132acd0cdb0302be69c68ce2b5678cee9e
owenavon <89999663+owenavon@users.noreply.github.com>
2024-03-22 10:59:21 -0400

    Repurposed js code to change the position of project covers upon page refresh and every 10 seconds on the index page

--- final/resources/js/script.js
+++ final/resources/js/script.js
@@ -1,49 +1,29 @@
-window.addEventListener('DOMContentLoaded', event => {
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+// Change position of each project cover image.
+const getRandomNumber = (function() {
+    let nums = [1,2,3];
+    let current = [];
+    
+    function rand(n) {
+        return (Math.random() * n)|0;
+    }
+    return function() {
+      if (!current.length) current = nums.slice();
+      return current.splice(rand(current.length), 1);
+    }
+}());
+const images = document.querySelectorAll('#gallery img');
 
-    };
+getRandomImages = () => {   
+    const imagesNums = [];
+    for (let i = 1; i < 4; i++) {
+        imagesNums.push(getRandomNumber());
+    }
+    images.forEach((img, index) => {
+        img.src = `./resources/images/index/${imagesNums[index]}.jpg`
+    })
+}
 
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
-});
+// Makes the images change order every 10 seconds.
+setInterval(() => {
+    getRandomImages()
+}, 10000);
