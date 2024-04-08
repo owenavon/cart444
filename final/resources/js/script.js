793d98ab5dff6a67835427090a7cecea97f74e2f
owenavon <89999663+owenavon@users.noreply.github.com>
2024-04-08 11:41:58 -0400

    Working on JS array for images, titles, and link

--- final/resources/js/script.js
+++ final/resources/js/script.js
@@ -1,7 +1,6 @@
-
-// Change position of each project cover image.
+// Assigns each image to a position in the array
 const getRandomNumber = (function() {
-    let nums = [1,2,3,4,5,6,7,8,9,10,11,12];
+    let nums = [[1, `Abandonded`, `./projects/abandoned.html`], [2, `Dearly Beloved`, `./projects/dearly-beloved.html`], [3, `Guydance`, `./projects/guydance.html`], [4, `Cafe Molo`, `./projects/cafe-molo.html`], [5, `Inverse`, `./projects/inverse.html`], [6, `Tractor Tipping`, `./projects/tractor-tipping.html`], [7, `Our Hidden Hills`, `./projects/our-hidden-hills.html`], [8, `Ski-Sons`, `./projects/ski-sons.html`], [9, `SPEED`, `./projects/speed.html`], [10, `Contactless Control`, `./projects/contactless-control.html`], [11, `Haptic-Freq`, `./projects/haptic-frew.html`], [12, `-> (Direction)`, `./projects/direction.html`]];
     let current = [];
     
     function rand(n) {
@@ -13,7 +12,10 @@ const getRandomNumber = (function() {
     }
 }());
 const images = document.querySelectorAll('#gallery-fade img');
+//const title = document.querySelectorAll('#gallery-fade h5');
+//const link = document.querySelectorAll('#gallery-fade a');
 
+// Loops through the 12 images
 getRandomImages = () => {   
     const imagesNums = [];
     
@@ -22,11 +24,19 @@ getRandomImages = () => {
     }
     
     images.forEach((img, index) => {
-        img.src = `./resources/images/index/${imagesNums[index]}.jpg`
+        img.src = `./resources/images/index/${imagesNums[index][0]}.jpg`
     })
+    
+//    title.forEach((h5, index) => {
+//        h5.innerHTML(imagesNums[index][1]); // innerText
+//    })
+    
+//    link.forEach((a, index) => {
+//        a.innerHTML(imagesNums[index][2]);
+//    })
 }
 
-// Makes the images change order every 10 seconds.
+// Changes the position of each project image every 10 seconds.
 setInterval(() => {
     getRandomImages()
 }, 10000);
